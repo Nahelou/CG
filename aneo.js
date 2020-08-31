7cb10e6a0e67e141e50de5bcac4b41d8d4cb3179
Quentin Nahélou <36446640+Nahelou@users.noreply.github.com>
2020-08-31 11:16:12 +0200

    Create aneo.js

---
+++ aneo.js
@@ -0,0 +1,18 @@
+let speed = +readline();
+const lightCount = +readline();
+const lights = [];
+
+for (let i = 0; i < lightCount; i++)
+    lights.push(readline().split(' ').map(Number));
+
+const isRed = (speed, dist, dur) =>
+    (18 * dist) % (10 * speed * dur) >= (5 * speed * dur);
+
+for(let i = 0; i < lightCount; i++) {
+    if (isRed(speed, lights[i][0], lights[i][1])) {
+        speed--;
+        i = -1;
+    }
+}
+console.log(speed);
+
