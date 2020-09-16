c9826f7ba8f7d317a6b3d69aa290cbd21a6d0e9c
Quentin Nahélou <36446640+Nahelou@users.noreply.github.com>
2020-09-16 18:01:42 +0200

    Update lowestTemperature.js

--- lowestTemperature.js
+++ lowestTemperature.js
@@ -1,7 +1,6 @@
+//The goal here is to have the tiniest code length
 readline();
-let t = readline().split(' ').map(Number).reduce(function(p, c) {
-  return (Math.abs(c) < Math.abs(p) ? c : Math.abs(c) == Math.abs(p)? Math.abs(c) : p);
-});
-(function(){
-    return t ? console.log(t) : console.log(0);
-})()
+let t=readline().split(' ').map(Number).reduce(function(p, c){
+  return(Math.abs(c)<Math.abs(p)?c:(Math.abs(c)==Math.abs(p)&&(c>0||p>0))?Math.abs(c):p
+        )});
+if(t){console.log(t)}else{console.log(0)}
